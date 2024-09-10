25ac0b1024cfb61c0e62c8877f7ee1d26e20369d
sa-ga-r <116742231+sa-ga-r@users.noreply.github.com>
2024-09-10 13:44:51 +0000

    firsthalf

--- imgdex.py
+++ imgdex.py
@@ -1,6 +1,13 @@
 import torch
 import streamlit as st
-import PIL
+from PIL import Image
+from torchvision import models, transforms
 
-print(f"Torch:{torch.__version__}")
-print(f"Streamlit:{st.__version__}")
+uploaded_img = st.file_uploader("Upload an image", type='jpg')
+img = Image.open(uploaded_img)
+st.image(img, caption="Uploaded image", use_column_width=True)
+
+model = models.resnet101(prettained = True)
+
+transform = transforms.Compose([transforms.Resize(256), transforms.CenterCrop(224), transforms.ToTensor(), transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
+
