45329c9992f743fe877dd20daa6692a329dd5f62
sa-ga-r <116742231+sa-ga-r@users.noreply.github.com>
2024-09-11 07:20:52 +0000

    full app

--- imgdex.py
+++ imgdex.py
@@ -1,13 +1,28 @@
-import torch
 import streamlit as st
+import torch
+import torchvision.models as models
+import torchvision.transforms as transforms
 from PIL import Image
-from torchvision import models, transforms
 
-uploaded_img = st.file_uploader("Upload an image", type='jpg')
-img = Image.open(uploaded_img)
-st.image(img, caption="Uploaded image", use_column_width=True)
+model = models.resnet101(pretrained = True)
+model.eval()
 
-model = models.resnet101(prettained = True)
+class_names = model.class_to_idx.keys()
 
-transform = transforms.Compose([transforms.Resize(256), transforms.CenterCrop(224), transforms.ToTensor(), transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
+transform = transforms.Compose([transforms.Resize(224), transforms.ToTensor(), transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
 
+def main():
+    st.title("Imgdex")
+    uploaded_file = st.file_uploader("Upload image...")
+    if uploaded_file is not None:
+        image = Image.open(uploaded_file)
+        image = transform(image).unsqueeze(0)
+        with torch.no_grad():
+            output = model(image)
+            _, pred = torch.max(output, 1)
+        predicted_class = class_names[pred.item()]
+        st.image(image.squeeze(0).permute(1, 2, 0))
+        st.write("Detected :", predicted_class)
+
+if __name__ == "__main__":
+    main()
